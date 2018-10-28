95b9009c64e9c9ae8127a4895c9be4636d60a46f
railsroger <railsroger@gmail.com>
2018-10-28 13:16:49 +0300

    Create Deck

---
+++ deck.rb
@@ -0,0 +1,22 @@
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = create_cards
+  end
+
+  protected
+
+  def create_cards
+    cards = []
+    %w(+ <3 ^ <>).each do |suit|
+      (2..10).each do |number|
+        cards << Card.new(suit, number)
+      end
+      %w(A K Q J).each do |namecard|
+        cards << Card.new(suit, namecard)
+      end
+    end
+    cards.shuffle!
+  end
+end
