9fd0db64c17f47d0351e86895be69a9efd1e17ba
railsroger <railsroger@gmail.com>
2018-10-28 13:02:39 +0300

    Create player

--- player.rb
+++ player.rb
@@ -1,8 +1,29 @@
 class Palayer
+  attr_accessor :cards, :name, :bank
 
-def initialize
-  @bank = 100
-  @player_cards
-end
+  def initialize(name, bank)
+    @name = name
+    @bank = bank
+    @cards = []
+  end
 
+  def rate(rate)
+    @bank -= rate
+  end
+
+  def win(rate)
+    @bank += rate
+  end
+
+  def total_cards
+    cards.size
+  end
+
+  def show_cards
+    if name != 'Dealer'
+      puts "Player: #{name} cards: #{cards * ' '}"
+    else
+      puts "Player: #{name} cards: #{cards.collect { |_card| _card = '*' } * ' '}"
+    end
+  end
 end
